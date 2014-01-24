144d33be0b0132eae1301788e9cbd196d6bef67d
TomShacham <shachamtom@gmail.com>
2014-01-24 10:46:17 +0000

    a look at creating attr_accessor style helpers co. _why

--- metaid.rb
+++ metaid.rb
@@ -1,36 +1,44 @@
 class Object
-   # The hidden singleton lurks behind everyone
-   def metaclass; class << self; self; end; end
-   def meta_eval &blk; metaclass.instance_eval &blk; end
+# The hidden singleton lurks behind everyone
+  def metaclass; class << self; self; end; end
+  def meta_eval &blk; metaclass.instance_eval &blk; end
 
-   # Adds methods to a metaclass
-   def meta_def name, &blk
-     meta_eval { define_method name, &blk }
-   end
+  # Adds methods to a metaclass
+  def meta_def name, &blk
+   meta_eval { define_method name, &blk }
+  end
 
-   # Defines an instance method within a class
-   def class_def name, &blk
-     class_eval { define_method name, &blk }
-   end
- end
+  # Defines an instance method within a class
+  def class_def name, &blk
+   class_eval { define_method name, &blk }
+  end
+end
 
-
-class MailTruck
-  def initialize(name, bhp)
-    @name, @bhp = name, bhp
-    class << self
-      def to_s
-        'hi im a singleton method'
+class Creature
+  def self.traits( *arr )
+    return @traits if arr.empty?
+    attr_accessor *arr
+    arr.each do |trait|
+      meta_def trait do |val|
+        @traits ||= {}
+        @traits[trait] = val
+      end
+    end
+    class_def :initialize do
+      self.class.traits.each do |k,v|
+        instance_variable_set( "@#{k}", v )
       end
     end
   end
 end
 
-m = MailTruck.new('m1',500)
-
-p m.to_s
-
-def m.a_singleton_method
-  p self
+class Rabbit < Creature
+  traits :bombs, :arrows
+  def a
+  end
 end
+
+p Creature.methods.first
+p Rabbit.singleton_methods
+
 
